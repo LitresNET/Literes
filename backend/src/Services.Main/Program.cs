7de20973859e252cc97f44cf887d655b7f4fbe44
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2024-03-22 15:08:34 +0300

    litres-103 initial commit

--- backend/src/Services.Main/Program.cs
+++ backend/src/Services.Main/Program.cs
@@ -39,6 +39,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionMiddleware>();
 
