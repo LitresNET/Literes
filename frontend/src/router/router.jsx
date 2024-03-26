cb2931fd6022e86dc4ae10e24c5fc3233be85682
Ruzan Valeev <61345590+coldezzt@users.noreply.github.com>
2024-03-26 18:02:25 +0300

    fix router

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -35,9 +35,6 @@ const routes = [
           element: <SearchPage />
         },
         {
-          path: ''
-        },
-        {
           path: 'subscription',
           element: <SubscriptionPage />
         },
