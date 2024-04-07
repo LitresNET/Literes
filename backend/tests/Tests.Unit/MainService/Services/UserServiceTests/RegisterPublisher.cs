5f519ff5721fb98f71ecc9567a0ed346a265290d
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2024-04-07 12:01:13 +0300

    litres-103 test(user-service-tests) create mocks

--- backend/tests/Tests.Unit/MainService/Services/UserServiceTests/RegisterPublisher.cs
+++ backend/tests/Tests.Unit/MainService/Services/UserServiceTests/RegisterPublisher.cs
@@ -7,20 +7,25 @@ using Moq;
 
 namespace Tests.MainService.Services.UserServiceTests;
 
-public class RegisterUser
+public class RegisterPublisher
 {
     private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
     private readonly Mock<UserManager<User>> _userManagerMock = new(
         Mock.Of<IUserStore<User>>(), 
         null, null, null, null, null, null, null, null);
+    private readonly Mock<IPublisherRepository> _publisherRepository = new();
     private readonly Mock<IConfiguration> _configurationMock = new();
-
+    
     private UserService UserService => new(
         _unitOfWorkMock.Object,
         _userManagerMock.Object,
         _configurationMock.Object
     );
-    
-    
-    
+
+    public RegisterPublisher()
+    {
+        _unitOfWorkMock
+            .Setup(unitOfWorkMock => unitOfWorkMock.GetRepository<Publisher>())
+            .Returns(_publisherRepository.Object);
+    }
 }
