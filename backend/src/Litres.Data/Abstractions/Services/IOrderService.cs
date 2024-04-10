894841cbc0f5a0b6963376ec39e455b9f572cb6e
Ruzan Valeev <61345590+coldezzt@users.noreply.github.com>
2024-04-10 19:32:45 +0300

    litres-105 fix(order-data) fix authorization problems

--- backend/src/Litres.Data/Abstractions/Services/IOrderService.cs
+++ backend/src/Litres.Data/Abstractions/Services/IOrderService.cs
@@ -4,6 +4,7 @@ namespace Litres.Data.Abstractions.Services;
 
 public interface IOrderService
 {
+    public Task<Order> GetById(long orderId);
     public Task<Order> CreateAsync(Order order);
     public Task<Order> DeleteAsync(long orderId);
 }
--- backend/src/Litres.Main/Controllers/OrderController.cs
+++ backend/src/Litres.Main/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using Litres.Data.Abstractions.Services;
 using Litres.Data.Dto.Requests;
 using Litres.Data.Dto.Responses;
 using Litres.Data.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Litres.Main.Controllers;
@@ -12,18 +13,32 @@ public class OrderController(
     IOrderService orderService,
     IMapper mapper): ControllerBase
 {
+    [Authorize]
     [HttpPost]
     public async Task<IActionResult> Create(OrderCreateRequestDto dto)
     {
+        if (long.TryParse(User.Claims.FirstOrDefault(c => c.Type == CustomClaimTypes.UserId)?.Value, out var userId))
+            return Unauthorized();
+        if (userId != dto.UserId)
+            return Forbid();
+        
         var order = mapper.Map<Order>(dto);
         var createdOrder = await orderService.CreateAsync(order);
         var response = mapper.Map<OrderCreateResponseDto>(createdOrder);
         return Ok(response);
     }
 
+    [Authorize]
     [HttpDelete("{orderId}")]
     public async Task<IActionResult> Delete(long orderId)
     {
+        if (long.TryParse(User.Claims.FirstOrDefault(c => c.Type == CustomClaimTypes.UserId)?.Value, out var userId))
+            return Unauthorized();
+        
+        var order = await orderService.GetById(orderId);
+        if (order.UserId != userId)
+            return Forbid();
+        
         var deletedOrder = await orderService.DeleteAsync(orderId);
         var response = mapper.Map<OrderCreateResponseDto>(deletedOrder);
         return Ok(response);
