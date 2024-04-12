034485879e2c932525ffd586cedd1b9605b76756
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2024-04-12 19:05:23 +0300

    litres-14 feature(authorization) add roles

--- backend/src/Litres.Main/Program.cs
+++ backend/src/Litres.Main/Program.cs
@@ -15,9 +15,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseLazyLoadingProxies()
         .UseSqlServer(builder.Configuration["Database:ConnectionString"]));
-builder.Services.AddDefaultIdentity<User>(options =>
+builder.Services.AddIdentity<User, IdentityRole<long>>(options =>
     options.User.RequireUniqueEmail = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -62,6 +63,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// For create roles
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<long>>>();
+    var roles = new[] { "Admin", "Moderator", "Publisher", "Member" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole<long>(role));
+        }
+    }
 }
 
 app.UseMiddleware<ExceptionMiddleware>();
