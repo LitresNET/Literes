b9436d31cb3088c6057a3b5e1400bf4401b15c6d
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2024-04-23 12:41:23 +0300

    litres-111 test(review-methods) intermediate commit

---
+++ backend/tests/Tests.Unit/MainService/Services/ReviewServiceTests/DeleteReview.cs
@@ -0,0 +1,25 @@
+﻿using Litres.Data.Abstractions.Repositories;
+using Litres.Data.Models;
+using Litres.Data.Repositories;
+using Litres.Main.Services;
+using Moq;
+
+namespace Tests.MainService.Services.ReviewServiceTests;
+
+public class DeleteReview
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+    private readonly Mock<IReviewRepository> _reviewRepositoryMock = new();
+
+    private ReviewService ReviewService => new(
+        _unitOfWorkMock.Object
+        );
+
+    public DeleteReview()
+    {
+        _unitOfWorkMock
+            .Setup(unitOfWorkMock => unitOfWorkMock.GetRepository<Review>())
+            .Returns(_reviewRepositoryMock.Object);
+    }
+    
+}
---
+++ backend/tests/Tests.Unit/MainService/Services/ReviewServiceTests/UpdateReview.cs
@@ -0,0 +1,23 @@
+﻿using Litres.Data.Abstractions.Repositories;
+using Litres.Data.Models;
+using Litres.Main.Services;
+using Moq;
+
+namespace Tests.MainService.Services.ReviewServiceTests;
+
+public class UpdateReview
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+    private readonly Mock<IReviewRepository> _reviewRepositoryMock = new();
+
+    private ReviewService ReviewService => new(
+        _unitOfWorkMock.Object
+    );
+
+    public UpdateReview()
+    {
+        _unitOfWorkMock
+            .Setup(unitOfWorkMock => unitOfWorkMock.GetRepository<Review>())
+            .Returns(_reviewRepositoryMock.Object);
+    }
+}
