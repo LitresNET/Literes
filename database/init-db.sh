fdf6be241bfd425ca7249d1e8c10f2c3305449d2
Who-car <aysulu2645@gmail.com>
2024-05-11 17:06:52 +0300

    litres-70 fix(dockerfile) fix database connection string

--- database/init-db.sh
+++ database/init-db.sh
@@ -5,7 +5,7 @@
 sleep 30s
 
 # Выполняем скрипт создания баз данных
-/opt/mssql-tools/bin/sqlcmd -S database -U sa -P _BEZ_Par0lya_007_ -i create-databases.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost/sqlexpress -U sa -P _BEZ_Par0lya_007_ -i create-databases.sql
 
 # Запускаем бесконечный цикл, чтобы контейнер оставался запущенным
 while true; do sleep 1d; done
