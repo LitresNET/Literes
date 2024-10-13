f08811d36e8734cfb6573a690b6b09ce8a23534a
Who-car <aysulu2645@gmail.com>
2024-10-13 23:24:15 +0300

    feat: update docker-compose to a stable version

--- backend/Litres.WebAPI/Program.cs
+++ backend/Litres.WebAPI/Program.cs
@@ -60,7 +60,6 @@ builder.Services.ConfigureServices(builder.Environment, builder.Configuration);
 
 var application = builder.Build();
 
-await application.AddMigrations();
 await application.AddIdentityRoles();
 
 if (application.Environment.IsDevelopment())
