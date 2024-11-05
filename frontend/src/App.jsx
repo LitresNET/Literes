bf25e3a9987dbd9fad3c92c91ad1097520240c84
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2024-11-05 18:15:58 +0300

    Revert "litres feature(frontend) initial chat commit"
    This reverts commit 65ccf63ea4fc850008b28ba12619549915f76403.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import router from './router/router';
 import {ToastContainer} from "react-toastify";
-import Chat from "./layouts/Chat/Chat.jsx";
 
 function App() {
     return (
         <>
             <RouterProvider router={router}></RouterProvider>
             <ToastContainer />
-            <Chat />
         </>
     );
 }
