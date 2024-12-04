2b8456b0fce92a0357b93e274f0e0ff381f969a3
Ruzan Valeev <61345590+coldezzt@users.noreply.github.com>
2024-12-04 13:47:22 +0300

    litres feature(backend) rewrite review service to cqrs

---
+++ backend/Litres.Application/Queries/Reviews/GetReview.cs
@@ -0,0 +1,9 @@
+﻿using Litres.Application.Dto;
+using Litres.Domain.Abstractions.Queries;
+
+namespace Litres.Application.Queries.Reviews;
+
+public class GetReview(long reviewId) : IQuery<ReviewDto>
+{
+    public long ReviewId { get; } = reviewId;
+}
