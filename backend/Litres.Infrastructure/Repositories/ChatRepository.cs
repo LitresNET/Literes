2b2737a95d4562cff6e27f5ddb13c14ecfa99fa6
Aysulu Gataullina <aysulu.gataullina@truesolv.com>
2024-12-15 23:08:34 +0300

    fix: docker-compose not building

--- backend/Litres.Infrastructure/Repositories/ChatRepository.cs
+++ backend/Litres.Infrastructure/Repositories/ChatRepository.cs
@@ -4,5 +4,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Litres.Infrastructure.Repositories;
 
-public class ChatRepository(ApplicationDbContext appDbContext) 
-    : Repository<Chat>(appDbContext), IChatRepository;
+public class ChatRepository(ApplicationDbContext appDbContext)
+    : Repository<Chat>(appDbContext), IChatRepository
+{
+    public Task<Chat?> GetBySessionIdAsync(string? chatSessionId)
+    {
+        return appDbContext.Chat
+            .Include(c => c.Messages)
+            .FirstOrDefaultAsync(c => c.SessionId == chatSessionId);
+    }
+    
+    public Task<Chat?> GetByUserIdAsync(long userId)
+    {
+        return appDbContext.Chat
+            .Include(c => c.Messages)
+            .FirstOrDefaultAsync(c => c.UserId == userId || c.AgentId == userId);
+    }
+
+    public Task<List<Chat>> GetByAgentIdAsync(long agentId)
+    {
+        return appDbContext.Chat
+            .Include(c => c.Messages)  
+            .Where(c => c.AgentId == agentId)
+            .GroupBy(c => c.UserId)  
+            .Select(g => g.OrderByDescending(c => c.Messages.OrderByDescending(m => m.SentDate).FirstOrDefault().SentDate)
+                .FirstOrDefault()) 
+            .ToListAsync();
+    }
+}
