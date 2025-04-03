d744f7af6c69ac5f55cfb4348acb6b54614d8882
Aysulu Gataullina <aysulu.gataullina@truesolv.com>
2025-04-04 01:20:49 +0300

    complete task

---
+++ backend/Litres.Application/Dto/Requests/CreateOrderDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Litres.Application.Dto.Requests;
+
+public class CreateOrderDto
+{
+    public long OrderId { get; set; }
+    public double Amount { get; set; }
+}
---
+++ backend/Litres.WebAPI/Services/PaymentEventService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using Litres.Application.Commands.Orders;
+using Litres.Application.Dto;
+using Litres.Domain.Abstractions.Commands;
+using Payment;
+
+namespace Litres.WebAPI.Services;
+
+public class PaymentEventService(ICommandDispatcher commandDispatcher) : Payment.PaymentEventService.PaymentEventServiceBase
+{
+    public override async Task<PaymentResponse> ProcessPaymentCompletion(PaymentCompletedEvent request, ServerCallContext context)
+    {
+        var updateOrderCommandDto = new OrderDto() {Id = int.Parse(request.OrderId), Status = "Completed"};
+        var updateOrderCommand = new UpdateOrderCommand(updateOrderCommandDto);
+        await commandDispatcher.DispatchReturnAsync<UpdateOrderCommand, OrderDto>(updateOrderCommand);
+        return new PaymentResponse { Success = true};
+    }
+
+    public override async Task<PaymentResponse> ProcessPaymentFailure(PaymentFailedEvent request, ServerCallContext context)
+    {
+        var updateOrderCommandDto = new OrderDto() {Id = int.Parse(request.OrderId), Status = "Failed"};
+        var updateOrderCommand = new UpdateOrderCommand(updateOrderCommandDto);
+        await commandDispatcher.DispatchReturnAsync<UpdateOrderCommand, OrderDto>(updateOrderCommand);
+        return new PaymentResponse { Success = false, Message = "Failed" };
+    }
+}
