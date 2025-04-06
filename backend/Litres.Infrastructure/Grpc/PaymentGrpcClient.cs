5278c2cb45ca5a8127c38780bb53158e6534c0d5
Who-car <85780480+Who-car@users.noreply.github.com>
2025-04-06 20:42:45 +0300

    complete task (#115)

---
+++ backend/Litres.Infrastructure/Grpc/PaymentGrpcClient.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Litres.Application.Abstractions.GrpcClients;
+using Litres.Application.Dto.Requests;
+using Litres.Application.Dto.Responses;
+using Payment;
+
+namespace Litres.Infrastructure.Grpc;
+
+public class PaymentGrpcClient(string url) : IPaymentClient
+{
+    public async Task<CreateOrderResponseDto> RegisterPaymentAsync(CreateOrderDto request)
+    {
+        using var channel = GrpcChannel.ForAddress(url);
+        var client = new PaymentRegistrationService.PaymentRegistrationServiceClient(channel);
+        var grpcRequest = new PaymentRegistrationRequest()
+        {
+            OrderId = request.OrderId.ToString(),
+            PaymentId = Guid.NewGuid().ToString(),
+            Amount = request.Amount,
+            Currency = "RUB",
+            ReturnUrl = "",
+            CancelUrl = ""
+        };
+        
+        try
+        {
+            var response = await client.RegisterPaymentAsync(grpcRequest);
+            return new CreateOrderResponseDto {Success = response.Success, PaymentRedirectUrl = response.PaymentUrl};
+        }
+        catch (RpcException ex)
+        {
+            throw new Exception($"gRPC error: {ex.Status.Detail}", ex);
+        }
+    }
+}
